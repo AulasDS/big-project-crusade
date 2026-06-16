c8c094d2fff6c26a71db8991092dcbb9c7bf285c
DzZ <oliveleonardo0404@gmail.com>
2026-06-16 14:55:55 -0300

    fix routers

--- api/src/routes/BibliotecaRouter.js
+++ api/src/routes/BibliotecaRouter.js
@@ -5,6 +5,7 @@ const BibliotecaController = require('../controllers/BibliotecaController');
 router.post('/', BibliotecaController.create);
 router.get('/', BibliotecaController.getAll);
 router.get('/:id', BibliotecaController.getById);
+router.put('/:id', BibliotecaController.update);
 router.delete('/:id', BibliotecaController.delete);
 
 module.exports = router;
